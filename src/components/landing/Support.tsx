eefd1482d1857e8505a4ea69d9bc3828c86c6916
Joseph Fugata <workwithjosephfugata@gmail.com>
2025-08-28 21:56:48 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="text-b

--- src/components/landing/Support.tsx
+++ src/components/landing/Support.tsx
@@ -30,7 +30,7 @@ export default function Support() {
               <div>
                 <CardTitle className="font-headline text-2xl">Messenger Chat</CardTitle>
                 <CardDescription className="text-base text-muted-foreground">
-                 Mag-message sa amin sa Messenger para sa mas mabilis na kasagutan.
+                  <a href="https://m.me/pinoyentrepreneur.me" target="_blank" rel="noopener noreferrer" className="underline">Mag-message sa amin sa Messenger para sa mas mabilis na kasagutan.</a>
                 </CardDescription>
               </div>
             </CardHeader>
